c43bc47ee6811d403060061afc15a82ba0f24c68
OlieHimmars <109204093+OlieHimmars@users.noreply.github.com>
2023-03-08 12:55:09 +0200

    item

--- src/components/ContactItem/ContactItem.js
+++ src/components/ContactItem/ContactItem.js
@@ -1,12 +1,12 @@
 import propTypes from 'prop-types';
 import { Item, DelButton } from './ContactItemStyled';
 
-const ContactItem = ({ id, name, number, onDeleteContact }) => {
+const ContactItem = ({ contactId, name, number, onDeleteContact }) => {
   return (
     <Item>
       <span>{name}:</span>
       <span>{number}</span>
-      <DelButton type="button" onClick={() => onDeleteContact(id)}>
+      <DelButton type="button" onClick={() => onDeleteContact(contactId)}>
         Delete
       </DelButton>
     </Item>
@@ -15,9 +15,9 @@ const ContactItem = ({ id, name, number, onDeleteContact }) => {
 
 ContactItem.propTypes = {
   name: propTypes.string.isRequired,
-  id: propTypes.string.isRequired,
+  contactId: propTypes.string.isRequired,
   number: propTypes.string.isRequired,
-  deleteContactHandler: propTypes.func.isRequired,
+  onDeleteContact: propTypes.func.isRequired,
 };
 
 export default ContactItem;
