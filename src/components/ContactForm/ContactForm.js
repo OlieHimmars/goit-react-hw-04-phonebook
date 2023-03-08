caddcd13ccf4c907043ec2dc1962927f139be203
OlieHimmars <109204093+OlieHimmars@users.noreply.github.com>
2023-03-08 12:09:47 +0200

    fix

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -33,6 +33,7 @@ export default function ContactForm({ onSubmit }) {
               onChange={onChangeName}
               type="text"
               name="name"
+              value={name}
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
@@ -46,6 +47,7 @@ export default function ContactForm({ onSubmit }) {
               onChange={onChangeNumber}
               type="tel"
               name="number"
+              value={number}
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               required
